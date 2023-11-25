ba353c77b8f625540ab406b030cb1b4a9526a516
afifShyam <afifaxelo12@gmail.com>
2023-11-25 20:23:52 +0800

    tak ingat

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:seniormatchpro_v1/index.dart';
 import 'package:flutter/material.dart';
 
@@ -13,17 +14,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
+      appBar: AppBar(
+        leadingWidth: 100,
+        leading: ElevatedButton(
+          child: const Text("Logout"),
           onPressed: () {
             FirebaseAuth.instance.signOut().then((value) {
-              print("Signed Out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
+              if (kDebugMode) {
+                print("Signed Out");
+              }
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SignInScreen()));
             });
           },
         ),
+        backgroundColor: Colors.yellowAccent,
+        title: const Text(
+          'Dashborad',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [],
+          ),
+        ],
       ),
     );
   }
--- lib/view/signup_screen.dart
+++ lib/view/signup_screen.dart
@@ -10,7 +10,6 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  final int id = 0;
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
   final TextEditingController _userNameTextController = TextEditingController();
