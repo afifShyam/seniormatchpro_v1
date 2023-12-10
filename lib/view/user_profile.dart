57a5884e8c569a6f94dbdfd571527bb193e0ba82
waiz01 <2020628062@student.uitm.edu.my>
2023-12-10 09:04:38 +0800

    2nd

--- lib/view/user_profile.dart
+++ lib/view/user_profile.dart
@@ -1,66 +1,78 @@
 import 'package:flutter/material.dart';
 
-// User model class (replace this with your actual user model)
-class UserProfile extends StatefulWidget{
-
+class UserProfile extends StatefulWidget {
   const UserProfile({Key? key, required this.name, required this.email}) : super(key: key);
-  
 
   final String name;
   final String email;
 
-  
-
-  
-
-   @override
+  @override
   _UserProfileState createState() => _UserProfileState();
 }
 
 class _UserProfileState extends State<UserProfile> {
-  // Hard-coded user data (replace this with your actual user data)
-final UserProfile user = UserProfile(name: 'John Doe', email: 'john.doe@example.com');
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(widget.name),
+              background: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/profile_image.jpg'),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  color: Colors.grey[200],
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 16),
+                      Text(
+                        'Name: ${widget.name}',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Email: ${widget.email}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 16),
+                      Text('Phone: 123-456-7890'),
+                      Text('Address: 123 Main St, City'),
+                      Text('Experience: 5 years'),
+                      Text('Age: 30'),
+                      Text('Gender: Female'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              // You can replace this with an image from the network or device
-              backgroundImage: AssetImage('assets/profile_image.jpg'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Name: ${user.name}',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Email: ${user.email}',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Add any action you want to perform when the button is pressed
-                print('Edit Profile Pressed');
-              },
-              child: Text('Edit Profile'),
-            ),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add any action you want to perform when the button is pressed
+          print('Edit Profile Pressed');
+        },
+        child: Icon(Icons.edit),
       ),
     );
   }
 }
 
-
+void main() {
+  runApp(MaterialApp(
+    home: UserProfile(name: 'John Doe', email: 'john.doe@example.com'),
+  ));
+}
